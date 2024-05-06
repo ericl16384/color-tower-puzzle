7f47996743e3a5ea9f2b91cbe79ca46c8d2a7d5d
Eric <ericl16384@gmail.com>
2024-05-06 12:36:23 -0700

    finished backtracking, and it solved as much as is possible :)

--- main.py
+++ main.py
@@ -39,10 +39,11 @@ print()
 # Backtracking
 
 
-def print_grid(grid):
-    for row in grid:
+def print_grid(height_map, grid):
+    for y, row in enumerate(grid):
         line = ""
-        for v in row:
+        for x, v in enumerate(row):
+            line += str(6 - height_map[y][x])
             line += ".ROYGBP"[v]
             line += " "
         print(line)
@@ -52,25 +53,48 @@ for y in range(6):
     grid.append([0]*6)
 # grid[0][1] = 4
 
-def move_is_safe(grid, row, col, num):
+def move_is_safe(height_map, remaining_pieces, grid, row, col, color):
 
-    # Check if we find the same num
+    # Check if we find the same color
     # in the similar row , we
     # return false
     for x in range(6):
-        if grid[row][x] == num:
+        if grid[row][x] == color:
             return False
 
-    # Check if we find the same num in
+    # Check if we find the same color in
     # the similar column , we
     # return false
     for x in range(6):
-        if grid[x][col] == num:
+        if grid[x][col] == color:
             return False
+    
+    # check if height for color is valid
+    if not remaining_pieces[color-1][height_map[row][col]]:
+        return False
 
     return True
 
-def solve_puzzle(grid, row, col):
+current_best_score = 0
+# current_best_grid = []
+
+def solve_puzzle(height_map, remaining_pieces, grid, row, col):
+    global current_best_score
+    # print_grid(height_map, grid)
+    # # print(row, col)
+    # print()
+    # for i, color in enumerate(remaining_pieces):
+    #     line = ""
+    #     line += ".ROYGBP"[i+1]
+    #     for j, h in enumerate(color):
+    #         if h:
+    #             line += str(6 - j)
+    #         else:
+    #             line += " "
+    #     print(line)
+    # input()
+    # print()
+
     # check if finished puzzle
     if row == 6 - 1 and col == 6:
         return True
@@ -80,6 +104,52 @@ def solve_puzzle(grid, row, col):
         row += 1
         col = 0
     
-    todo
+    # recurse if cell is finished
+    if grid[row][col] > 0:
+        return solve_puzzle(height_map, remaining_pieces, grid, row, col + 1)
+    
+    for color in range(1, 6+1):
+        if move_is_safe(height_map, remaining_pieces, grid, row, col, color):
+            grid[row][col] = color
+            remaining_pieces[color-1][height_map[row][col]] = False
 
-print_grid(grid)
+            if solve_puzzle(height_map, remaining_pieces, grid, row, col + 1):
+                return True
+
+            if row * 6 + col + 1 > current_best_score:
+                current_best_score = row * 6 + col + 1
+                # current_best_grid = []
+                # for row in grid:
+                #     current_best_grid.append(row.copy())
+                print("score", current_best_score)
+                print_grid(height_map, grid)
+                print()
+                # input()
+            # print("score", row * 6 + col)
+            # print_grid(height_map, grid)
+            # print()
+        
+            # assumption was wrong, remove value
+            # also revert remaining height for color
+            grid[row][col] = 0
+            remaining_pieces[color-1][height_map[row][col]] = True
+            # print("backtrack")
+    
+    return False
+
+# print_grid(grid)
+heights_remaining = []
+for i in range(6):
+    heights_remaining.append([True]*6)
+
+import time
+start_time = time.time()
+solve_puzzle(board_obstacles, heights_remaining, grid, 0, 0)
+end_time = time.time()
+
+print("FINISHED")
+print_grid(board_obstacles, grid)
+
+print()
+print("seconds elapsed", end_time - start_time)
+# print(grid)
